6a922a660b092475348a62531f0b1d944ea12bb3
Alexander Oluwaseun Kwesi <seankwesi24@googlemail.com>
2025-05-25 01:32:36 +0100

    Update run.py

--- run.py
+++ run.py
@@ -3,5 +3,15 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
+    if __name__ == '__main__':
+    # Use 'spawn' on platforms like Windows; 'fork' is okay on UNIX.
+    if os.name != 'nt':
+        multiprocessing.set_start_method("fork", force=True)
+
+    sem = multiprocessing.Semaphore(1)
+    process = multiprocessing.Process(target=worker, args=(sem,))
+    process.start()
+    process.join()
+
     app.run(debug=True)
 
